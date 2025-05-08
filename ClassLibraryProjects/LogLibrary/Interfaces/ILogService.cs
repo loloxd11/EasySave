8a81199c59bf2111dd74283f8a46636c0ae4177f
loloxd11 <79612963+loloxd11@users.noreply.github.com>
2025-05-08 12:29:19 +0200

    Add LogLibrary to porject and in the dependncy

---
+++ ClassLibraryProjects/LogLibrary/Interfaces/ILogService.cs
@@ -0,0 +1,6 @@
+﻿public interface ILogService
+{
+    void LogFileTransfer(string jobName, string sourcePath, string targetPath, long fileSize, long transferTime);
+    string GetDailyLogFilePath(DateTime date);
+    bool IsLogServiceReady();
+}
---
+++ ClassLibraryProjects/LogLibrary/Utils/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿public static class FileHelper
+{
+    public static bool EnsureDirectoryExists(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+            return true;
+        }
+        return false;
+    }
+
+    public static void AppendToFile(string filePath, string content)
+    {
+        File.AppendAllText(filePath, content + "\n");
+    }
+
+    public static string ReadAllText(string filePath) => File.ReadAllText(filePath);
+    public static bool FileExists(string filePath) => File.Exists(filePath);
+}
