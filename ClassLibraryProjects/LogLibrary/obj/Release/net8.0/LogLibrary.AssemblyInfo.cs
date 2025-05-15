95fd3a1d27da1d0364a6d048b8d1a63cc83d8861
loloxd11 <leclercappereloan@gmail.com>
2025-05-15 15:04:54 +0200

    Bug fix with pagination Log in Json and XML

--- ClassLibraryProjects/LogLibrary/obj/Release/net8.0/LogLibrary.AssemblyInfo.cs
+++ ClassLibraryProjects/LogLibrary/obj/Release/net8.0/LogLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828febcc860a89f3b9e52793bfa2f7db45aec2ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4d8c24690a026442211834fb8b824afd765d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
