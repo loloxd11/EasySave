fd5dfedba640db7f272ffc0ddcf9716c3d3f3428
loloxd11 <leclercappereloan@gmail.com>
2025-05-19 00:05:39 +0200

    Add RealyCommand, update ViewModel and View, change between view and add Backup Job to config.json

--- EasySave/ViewModels/LanguageViewModel.cs
+++ EasySave/ViewModels/LanguageViewModel.cs
@@ -5,19 +5,40 @@ using System.Threading;
 
 namespace EasySave.ViewModels
 {
-    // Singleton pour LanguageViewModel (partagé globalement)
+    /// <summary>
+    /// Singleton class for managing application language settings globally.
+    /// Implements INotifyPropertyChanged to notify UI of language changes.
+    /// </summary>
     public class LanguageViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Event triggered when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // Singleton instance of the LanguageViewModel.
         private static readonly LanguageViewModel _instance = new LanguageViewModel();
+
+        /// <summary>
+        /// Public accessor for the singleton instance.
+        /// </summary>
         public static LanguageViewModel Instance => _instance;
 
+        // ResourceManager to manage language resource files.
         private ResourceManager _resourceManager;
+
+        // Current language of the application.
         private string _currentLanguage;
+
+        /// <summary>
+        /// Gets the current language of the application.
+        /// </summary>
         public string CurrentLanguage => _currentLanguage;
 
-        // Constructeur privé pour le singleton
+        /// <summary>
+        /// Private constructor to enforce singleton pattern.
+        /// Initializes the default language to English.
+        /// </summary>
         private LanguageViewModel()
         {
             _currentLanguage = "english";
@@ -25,6 +46,12 @@ namespace EasySave.ViewModels
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
         }
 
+        /// <summary>
+        /// Indexer to retrieve localized strings by key.
+        /// If the key is not found, returns the key wrapped in exclamation marks.
+        /// </summary>
+        /// <param name="key">The key for the localized string.</param>
+        /// <returns>The localized string or a placeholder if not found.</returns>
         public string this[string key]
         {
             get
@@ -34,6 +61,11 @@ namespace EasySave.ViewModels
             }
         }
 
+        /// <summary>
+        /// Changes the application's language and updates the ResourceManager and culture.
+        /// Notifies the UI of the language change.
+        /// </summary>
+        /// <param name="culture">The culture identifier (e.g., "french", "english").</param>
         public void ChangeLanguage(string culture)
         {
             switch (culture)
@@ -50,6 +82,8 @@ namespace EasySave.ViewModels
                     _currentLanguage = "english";
                     break;
             }
+
+            // Notify UI of the CurrentLanguage property change and refresh all bindings.
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentLanguage)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
         }
--- easysave/Commands/RelayCommand.cs
+++ easysave/Commands/RelayCommand.cs
@@ -3,56 +3,103 @@ using System.Windows.Input;
 
 namespace EasySave.Commands
 {
+    /// <summary>  
+    /// A generic command implementation that allows binding actions to UI elements.  
+    /// This class is used to encapsulate an Action delegate and an optional condition (Func<bool>)  
+    /// to determine whether the command can execute.  
+    /// </summary>  
     public class RelayCommand : ICommand
     {
         private readonly Action _execute;
         private readonly Func<bool> _canExecute;
 
+        /// <summary>  
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.  
+        /// </summary>  
+        /// <param name="execute">The action to execute when the command is invoked.</param>  
+        /// <param name="canExecute">Optional condition to determine if the command can execute.</param>  
+        /// <exception cref="ArgumentNullException">Thrown if the execute parameter is null.</exception>  
         public RelayCommand(Action execute, Func<bool> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
+        /// <summary>  
+        /// Occurs when changes occur that affect whether the command should execute.  
+        /// </summary>  
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>  
+        /// Determines whether the command can execute in its current state.  
+        /// </summary>  
+        /// <param name="parameter">Data used by the command. Not used in this implementation.</param>  
+        /// <returns>True if the command can execute; otherwise, false.</returns>  
         public bool CanExecute(object parameter)
         {
             return _canExecute == null || _canExecute();
         }
 
+        /// <summary>  
+        /// Executes the command.  
+        /// </summary>  
+        /// <param name="parameter">Data used by the command. Not used in this implementation.</param>  
         public void Execute(object parameter)
         {
             _execute();
         }
     }
 
+    /// <summary>  
+    /// A generic command implementation that supports parameterized actions.  
+    /// This class is used to encapsulate an Action<T> delegate and an optional condition (Predicate<T>)  
+    /// to determine whether the command can execute.  
+    /// </summary>  
+    /// <typeparam name="T">The type of the parameter passed to the command.</typeparam>  
     public class RelayCommand<T> : ICommand
     {
         private readonly Action<T> _execute;
         private readonly Predicate<T> _canExecute;
 
+        /// <summary>  
+        /// Initializes a new instance of the <see cref="RelayCommand{T}"/> class.  
+        /// </summary>  
+        /// <param name="execute">The action to execute when the command is invoked.</param>  
+        /// <param name="canExecute">Optional condition to determine if the command can execute.</param>  
+        /// <exception cref="ArgumentNullException">Thrown if the execute parameter is null.</exception>  
         public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
+        /// <summary>  
+        /// Occurs when changes occur that affect whether the command should execute.  
+        /// </summary>  
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>  
+        /// Determines whether the command can execute in its current state.  
+        /// </summary>  
+        /// <param name="parameter">The parameter passed to the command.</param>  
+        /// <returns>True if the command can execute; otherwise, false.</returns>  
         public bool CanExecute(object parameter)
         {
             return _canExecute == null || _canExecute((T)parameter);
         }
 
+        /// <summary>  
+        /// Executes the command.  
+        /// </summary>  
+        /// <param name="parameter">The parameter passed to the command.</param>  
         public void Execute(object parameter)
         {
             _execute((T)parameter);
